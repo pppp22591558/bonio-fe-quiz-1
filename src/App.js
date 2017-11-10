6c9af522a4e33120c1c439afe7bb51336d5bac8f
pppp22591558 <pppp29654213@gmail.com>
2017-11-10 14:04:38 +0800

    initialization

--- src/App.js
+++ src/App.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const COLUMN = 10;
+const LIMIT = 5;
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.column = props.column || COLUMN;
+    this.size = Math.pow(this.column, 2);
+    this.limit = props.limit || LIMIT ;
+    const occupied = this.pickNonRepeatingNumbers(this.size, this.limit);
+    const lands = new Array(this.size).fill(0).map((v, i) => {
+      if (occupied.indexOf(i) > 0) return 1;
+      return 0;
+    });
+    this.lands = lands;
+    this.occupied = occupied;
+  }
+  state = {
+    chosenLands: [],
+    chosenAndOccupiesLands: [],
+  }
+  pickNonRepeatingNumbers(range, limit) {
+    let i = limit;
+    const pool = new Array(range).fill(0).map((v, i) => i);
+    const randomNumbers = [];
+    while (i-- > 0) {
+      const index = Math.floor(Math.random() * pool.length)
+      randomNumbers.push(pool[index]);
+      pool.splice(index, 1);
+    }
+    return randomNumbers;
+  }
+  selectLand(i) {
+    this.setState(prevState => {
+      return {
+        chosenLands: [...prevState.chosenLands, i],
+        ...this.occupied.indexOf(i) > -1 ? {
+          chosenAndOccupiesLands: [...prevState.chosenAndOccupiesLands, i],
+        } : {},
+      };
+    });
+  }
   render() {
+    const { chosenLands, chosenAndOccupiesLands } = this.state;
+    if (chosenAndOccupiesLands.length === this.limit) return (
+      <div className="App"><h1 style={{ width: '100%' }}>YOU WIN!!!</h1></div>
+    )
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          this.lands.map((land, i) => {
+            const isChosen = chosenLands.indexOf(i) > -1;
+            const isOccupied = chosenAndOccupiesLands.indexOf(i) > -1;
+            return (
+              <div
+                style={{ flexBasis: `${100 / this.column}%` }}
+                className={`land${isChosen ? ' chosen' : ''}${isOccupied ? ' occupied' : ''}`}
+                key={i}
+                onClick={() => {
+                  if (isChosen) return;
+                  this.selectLand(i, land)
+                }}
+              />
+            );
+          })
+        }
       </div>
     );
   }
