c80e36560e1b83b32d6c51b8d421185b7cb7efff
pppp22591558 <pppp29654213@gmail.com>
2017-11-10 14:37:42 +0800

    add comments

--- src/App.js
+++ src/App.js
@@ -50,23 +50,38 @@ class App extends Component {
     )
     return (
       <div className="App">
-        {
-          this.lands.map((land, i) => {
-            const isChosen = chosenLands.indexOf(i) > -1;
-            const isOccupied = chosenAndOccupiesLands.indexOf(i) > -1;
-            return (
-              <div
-                style={{ flexBasis: `${100 / this.column}%` }}
-                className={`land${isChosen ? ' chosen' : ''}${isOccupied ? ' occupied' : ''}`}
-                key={i}
-                onClick={() => {
-                  if (isChosen) return;
-                  this.selectLand(i, land)
-                }}
-              />
-            );
-          })
-        }
+        <div style={{ marginBottom: 20 }}>
+          拆炸彈遊戲題目：請實現如以下5x5的土地<br/>
+          預設埋藏5顆炸彈<br/>
+          點到沒有炸彈的土地會顯示綠色<br/>
+          拆到炸彈會顯示紅色<br/>
+          拆光了會顯示勝利！<br/>
+        </div>
+        <div className="lands">
+          {
+            this.lands.map((land, i) => {
+              const isChosen = chosenLands.indexOf(i) > -1;
+              const isOccupied = chosenAndOccupiesLands.indexOf(i) > -1;
+              return (
+                <div
+                  style={{ flexBasis: `${100 / this.column}%` }}
+                  className={`land${isChosen ? ' chosen' : ''}${isOccupied ? ' occupied' : ''}`}
+                  key={i}
+                  onClick={() => {
+                    if (isChosen) return;
+                    this.selectLand(i, land)
+                  }}
+                />
+              );
+            })
+          }
+        </div>
+        <div style={{ marginTop: 20 }}>
+          延伸問題：<br/>
+          Readability   - 你的程式碼是否易讀、簡潔、DRY，建構函式的邏輯是否易懂，很好解釋你的邏輯<br/>
+          Reusability   - 如果要讓地圖改變成10x10，埋10顆炸彈，你的程式碼需要變動多少<br/>
+          Extensibility - 如果要實時配對對手，讓對手選擇埋藏炸彈的位置，你負責拆，你的程式碼會如何變動<br/>
+        </div>
       </div>
     );
   }
